857e91eb52347dd830e706336ec9591c893997d6
Kartik Kedia <kartikkedia011@gmail.com>
2023-08-17 23:14:20 +0530

    Update Post.js

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -6,7 +6,7 @@ export default function Post({_id, title, summary, cover, content, createdAt, au
         <div className="post">
             <div className="image">
                 <Link to={'/post/'+_id}>
-                    <img src={'http://localhost:4000/' + cover} />
+                    <img src={'https://blogic2.onrender.com/' + cover} />
                 </Link>
             </div>
             <div className="texts">

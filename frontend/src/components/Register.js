ae24f911018610f97d169aef71885e9f9be97a18
Kartik Kedia <kartikkedia011@gmail.com>
2023-08-17 22:25:33 +0530

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -8,7 +8,7 @@ export default function Register() {
     async function Register(ev) {
         ev.preventDefault();
 
-        const response = await fetch('http://localhost:4000/register', {
+        const response = await fetch('https://blogic2.onrender.com/register', {
             method: 'POST',
             body: JSON.stringify({ userName, password }),
             headers: { 'Content-Type': 'application/json' },

4dc11e0aa4dee75a3f806bf2f917c54a62b7b8ec
Kartik Kedia <kartikkedia011@gmail.com>
2023-08-17 22:59:17 +0530

    Update CreatePost.js

--- frontend/src/components/CreatePost.js
+++ frontend/src/components/CreatePost.js
@@ -19,7 +19,7 @@ export default function CreatePost() {
         data.set('content', content)
         data.set('files', files[0])
 
-        const response = await fetch('http://localhost:4000/post', {
+        const response = await fetch('https://blogic2.onrender.com/post', {
             method: 'POST',
             body: data,
             credentials: 'include',

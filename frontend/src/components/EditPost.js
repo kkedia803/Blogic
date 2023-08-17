a49bc8c0f000ed0cacf2797982a40a423cb8194b
Kartik Kedia <kartikkedia011@gmail.com>
2023-08-17 22:59:55 +0530

    Update EditPost.js

--- frontend/src/components/EditPost.js
+++ frontend/src/components/EditPost.js
@@ -11,7 +11,7 @@ export default function EditPost() {
     const [redirect, setRedirect] = useState(false)
 
     useEffect(()=>{
-        fetch('http://localhost:4000/post/'+id)
+        fetch('https://blogic2.onrender.com/post/'+id)
         .then(response=>{
             response.json().then(postInfo=>{
                 setTitle(postInfo.title);
@@ -34,7 +34,7 @@ export default function EditPost() {
         data.set('files', files[0])
         data.set('id', id);
 
-        const response = await fetch('http://localhost:4000/post', {
+        const response = await fetch('https://blogic2.onrender.com/post', {
             method: 'PUT',
             body: data,
             credentials: 'include',
